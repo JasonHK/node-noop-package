613d8478fabed7726659a44468996efb3ed86f28
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-18 18:56:11 +0800

    test: added more tests for `noop`

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -29,7 +29,6 @@ describe(
                                     "- `Function.prototype.apply()`",
                                     () =>
                                     {
-                                        expect(noop.apply()).toBe(noop);
                                         expect(Function.prototype.apply.call(noop)).toBe(noop);
                                     });
                         
@@ -37,18 +36,14 @@ describe(
                                     "- `Function.prototype.bind()`",
                                     () =>
                                     {
-                                        const binded1 = noop.bind();
-                                        expect(binded1()).toBe(noop);
-                
-                                        const binded2 = Function.prototype.bind.call(noop) as INoop;
-                                        expect(binded2(noop)).toBe(noop);
+                                        const binded = Function.prototype.bind.call(noop) as INoop;
+                                        expect(binded(noop)).toBe(noop);
                                     });
                         
                                 test(
                                     "- `Function.prototype.call()`",
                                     () =>
                                     {
-                                        expect(noop.call()).toBe(noop);
                                         expect(Function.prototype.call.call(noop)).toBe(noop);
                                     });
                 
@@ -81,6 +76,84 @@ describe(
                                     () =>
                                     {
                                         expect(Reflect.construct(noop as unknown as Function, [])).toBe(noop);
+                                    });
+                            });
+                    });
+
+                describe(
+                    "getPrototypeOf(target)",
+                    () =>
+                    {
+                        describe(
+                            "Case: Returns the proxy itself",
+                            () =>
+                            {
+                                test(
+                                    "- `Object.getPrototypeOf()`",
+                                    () =>
+                                    {
+                                        expect(Object.getPrototypeOf(noop)).toBe(noop);
+                                    });
+
+                                test(
+                                    "- `Reflect.getPrototypeOf()`",
+                                    () =>
+                                    {
+                                        expect(Reflect.getPrototypeOf(noop)).toBe(noop);
+                                    });
+
+                                test(
+                                    "- `__proto__`",
+                                    () =>
+                                    {
+                                        expect(noop.__proto__).toBe(noop);
+                                    });
+
+                                test(
+                                    "- `Object.prototype.isPrototypeOf()`",
+                                    () =>
+                                    {
+                                        expect(Object.prototype.isPrototypeOf.call(noop, noop)).toBe(true);
+                                    });
+
+                                test(
+                                    "- `instanceof`",
+                                    () =>
+                                    {
+                                        expect(noop instanceof noop).toBe(true);
+                                    });
+                            });
+                    });
+
+                describe(
+                    "setPrototypeOf(target, prototype)",
+                    () =>
+                    {
+                        describe(
+                            "Case: Returns `true`",
+                            () =>
+                            {
+                                test(
+                                    "- `Object.setPrototypeOf()`",
+                                    () =>
+                                    {
+                                        expect(
+                                            () =>
+                                            {
+                                                expect(Object.setPrototypeOf(noop, null)).toBe(noop);
+                                                expect(Object.setPrototypeOf(noop, {})).toBe(noop);
+                                                expect(Object.setPrototypeOf(noop, noop)).toBe(noop);
+                                            })
+                                            .not.toThrow();
+                                    });
+
+                                test(
+                                    "- `Reflect.setPrototypeOf()`",
+                                    () =>
+                                    {
+                                        expect(Reflect.setPrototypeOf(noop, null)).toBe(true);
+                                        expect(Reflect.setPrototypeOf(noop, {})).toBe(true);
+                                        expect(Reflect.setPrototypeOf(noop, noop)).toBe(true);
                                     });
                             });
                     });
